19f7f4cace4dd1d988f3b70e2db4bdc8bde626bf
Dmitry Vislov <vdv73rus@gmail.com>
2014-12-18 10:43:18 +0300

    Base datepicker functionality added.

--- js/app/controllers/booking.js
+++ js/app/controllers/booking.js
@@ -12,30 +12,59 @@ var app = angular.module('booking', ['ui.bootstrap']);
 app.controller('BookingCtrl', ['$scope', function($scope) {
 
   $scope.rooms = rooms;
-  $scope.days = 1;
 
   $scope.init = function() {
     $scope.roomPrice = $scope.rooms[0].value;
+
+    $scope.startDate = moment();
+    $scope.finishDate = moment().add(1, 'day');
+
+    $scope.calcDuration();
     $scope.calcTotal();
   };
 
   $scope.calcTotal = function() {
-    $scope.totalPrice = $scope.roomPrice * $scope.days;
+    $scope.calcDuration();
+    $scope.totalPrice = $scope.roomPrice * $scope.duration;
   };
 
   $scope.openStartDate = function($event) {
     $event.preventDefault();
     $event.stopPropagation();
 
+    $scope.openedFinishDate = false;
     $scope.openedStartDate = true;
   };
 
-  $scope.openFinishedDate = function($event) {
+  $scope.openFinishDate = function($event) {
     $event.preventDefault();
     $event.stopPropagation();
 
-    $scope.openedStartDate = true;
+    $scope.openedOpenDate = true;
+    $scope.openedFinishDate = true;
   };
+
+  $scope.datesFormat = function() {
+    $scope.startDateFormatted = "C " + moment($scope.startDate).format('DD.MM.YYYY') + " (14:00)";
+    $scope.finishDateFormatted = "По " + moment($scope.finishDate).format('DD.MM.YYYY') + " (12:00)";
+  };
+
+  $scope.calcDuration = function() {
+    var startRoundDay = moment($scope.startDate).startOf('day');
+    var finishRoundDay = moment($scope.finishDate).startOf('day');
+
+    $scope.duration = finishRoundDay.diff(startRoundDay, 'days');
+  };
+
+  $scope.$watch('startDate', function() {
+    $scope.calcTotal();
+    $scope.datesFormat();
+  });
+
+  $scope.$watch('finishDate', function() {
+    $scope.calcTotal();
+    $scope.datesFormat();
+  });
 
   $scope.init();
 
